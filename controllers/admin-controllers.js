5b34a11bd2df30bdcf277216b9a3baf4dde52587
Hieunm54 <hieunm.542@gmail.com>
2021-08-28 10:52:35 +0700

    finished edit, update functionality

--- controllers/admin-controllers.js
+++ controllers/admin-controllers.js
@@ -1,74 +1,94 @@
-import Product from '../models/product.js'
-import Cart from '../models/cart.js'
+import Product from "../models/product.js";
+import Cart from "../models/cart.js";
 
 class AdminController {
-    // [GET] /admin/add-product
-    getAddProduct = (req, res, next) => {
-        res.render("admin/add-product", { title: "Shop", path: "/admin/add-product" });
-    };
+	// [GET] /admin/add-product
+	getAddProduct = (req, res, next) => {
+		res.render("admin/add-product", {
+			title: "Shop",
+			path: "/admin/add-product",
+		});
+	};
 
-    // [GET] /admin/products
-    getAdminProduct = (req, res, next) => {
-        Product.fetchAll()
-            .then((products) => {
-                res.render("admin/products", {
-                    title: "My Shop",
-                    text: 'Welcome to our shop',
-                    products: products,
-                    path: "/admin/products",
-                });
-            })
-            .catch((err) => {
-                console.log(err);
-            });    
-    }
+	// [GET] /admin/products
+	getAdminProduct = (req, res, next) => {
+		Product.fetchAll()
+			.then((products) => {
+				res.render("admin/products", {
+					title: "My Shop",
+					text: "Welcome to our shop",
+					products: products,
+					path: "/admin/products",
+				});
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
 
-    // [POST ] /admin/add-product
-    postProduct = (req, res, next) => {
-        const {title,imgUrl,price,description} = req.body;
-        const product = new Product(title, imgUrl, price, description);
-        product.save()
-            .then(result =>{
-                console.log('product add: ',result);
-                res.redirect("/");
-            })
-            .catch(err =>{
-                console.log(err);
-            });
-    };
+	// [POST ] /admin/add-product
+	postProduct = (req, res, next) => {
+		const { title, imgUrl, price, description } = req.body;
+		const product = new Product(title, imgUrl, +price, description);
+		product
+			.save()
+			.then((result) => {
+				console.log("product add: ", result);
+				res.redirect("/");
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
 
-    // [GET] /admin/edit-product/:id
-    getEditProduct = (req, res, next) => {
-        const id = req.params.id; 
-        const editingMode = Boolean(req.query.edit);
-        if( !editingMode){
-            return res.redirect('/');
-        }
-        Product.fetchProductById(id, (product) => {
-            res.render("admin/edit-product",{ 
-                title: 'Edit Page',
-                product,
-                path: '/admin/edit-product'
-            })
-        })
-    }
+	// [GET] /admin/edit-product/:id
+	getEditProduct = (req, res, next) => {
+		const id = req.params.id;
+		const editingMode = Boolean(req.query.edit);
+		if (!editingMode) {
+			return res.redirect("/");
+		}
+		Product.fetchProductById(id)
+			.then((product) => {
+				res.render("admin/edit-product", {
+					title: "Edit Page",
+					product,
+					path: "/admin/edit-product",
+				});
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
 
-    // [PUT] /admin/edit-product
-    updateProduct = (req, res, next) => {
-        const id = req.body.id;
-        const {title,imgUrl,price,description} = req.body;
+	// [PUT] /admin/edit-product
+	updateProduct = (req, res, next) => {
+		const id = req.body.id;
+		const { title, imgUrl, price, description } = req.body;
 
-        Product.updateProduct(id,{id: id,title: title, imgUrl: imgUrl, price: +price,description: description});
-        res.redirect('/admin/products');
-    }
+		Product.updateProduct(
+			id,
+			{
+				_id: id,
+				title: title,
+				imgUrl: imgUrl,
+				price: +price,
+				description: description,
+			},
+			() => {
+				res.redirect("/admin/products");
+			}
+		);
+	};
 
-    // [DELETE] /admin/delete-product
-    deleteProduct = (req, res, next) => {
-        const id = req.body.id;
-        // Cart.deleteProduct(id);
-        Product.deleteProduct(id);
-        res.redirect('/admin/products');
-    }
+	// [DELETE] /admin/delete-product
+	deleteProduct = (req, res, next) => {
+		const id = req.body.id;
+		// Cart.deleteProduct(id);
+		Product.deleteProduct(id, () => {
+			res.redirect("/admin/products");
+		});
+	};
 }
 
 export default AdminController;
--- models/product.js
+++ models/product.js
@@ -3,9 +3,9 @@ import path from "path";
 import dirname from "../util/path.js";
 // import ObjectId from 'mongodb'
 
-import Cart from './cart.js';
+import Cart from "./cart.js";
 
-import {getDb} from '../util/database.js'
+import { getDb } from "../util/database.js";
 import { ObjectId } from "bson";
 
 const p = path.join(dirname, "data", "product.json");
@@ -31,79 +31,115 @@ class Product {
 	// save a product
 	save() {
 		const db = getDb();
-		return db.collection('products').insertOne(this);
+		return db.collection("products").insertOne(this);
 	}
 
 	// get all product
 	static fetchAll() {
 		const db = getDb();
-		return db.collection('products')
+		return db
+			.collection("products")
 			.find({})
 			.toArray()
-			.then(products => {
+			.then((products) => {
 				return products;
 			})
-			.catch(err => {
+			.catch((err) => {
 				throw err;
-			})
+			});
 	}
 
 	static fetchProductById(id) {
 		const db = getDb();
-		return db.collection('products')
-				.findOne({_id: ObjectId(id)})
-				.then(product => {
-					console.log('Fetching product: ' + product);
-					return product;
-				})
-				.catch(err => {
-					console.log(err);
-				})
-	}
-
-	static updateProduct(id, updateProduct) {
-		// lay ra product do
-		getProductFromFile((data) => {
-			const productIndex = data.findIndex((product) => product.id === id);
-			if (productIndex === -1) {
-				// console.log('update fails: ', productIndex);
-				return new Error();
-			}
-
-			//update data in cart.json
-			if(data[productIndex].price !== updateProduct.price){
-				Cart.updateTotalPrice(productIndex,data[productIndex].price, updateProduct.price);
-			}
-			
-			//update lai data
-			data[productIndex] = updateProduct;
-			const newData = [...data];
-
-			// write lai vao file product.json
-			fs.writeFile(p, JSON.stringify(newData), (err) => {
+		return db
+			.collection("products")
+			.findOne({ _id: ObjectId(id) })
+			.then((product) => {
+				console.log("Fetching product: " + product);
+				return product;
+			})
+			.catch((err) => {
 				console.log(err);
 			});
-		});
 	}
 
-	static deleteProduct = (id) =>{
-		// get product
-		getProductFromFile((data) =>{
-			const newData = [...data];
-			const deleteProduct = newData.find( p => p.id === id); 
-			const newProducts = newData.filter(product => product.id !== id);
-			fs.writeFile(p, JSON.stringify(newProducts), (err) => {
-				if(!err) {
-					Cart.deleteProduct(id,+deleteProduct.price);
-				}else{
-					console.log(err);
-				}
+	static updateProduct(id, updateProduct, cb) {
+		// lay ra product do
+		const db = getDb();
+		let query = { _id: ObjectId(id) };
+		let newValues = {
+			$set: {
+				title: updateProduct.title,
+				imgUrl: updateProduct.imgUrl,
+				price: updateProduct.price,
+				description: updateProduct.description,
+			},
+		};
+		db.collection("products")
+			.updateOne(query, newValues)
+			.then(() => {
+				console.log("Update successful");
+				cb();
 			})
-		})
+			.catch((err) => {
+				console.log(err);
+			});
 
+		//update data in cart.json- <de_sau>
+
+		//update lai data
+
+		// getProductFromFile((data) => {
+		// 	const productIndex = data.findIndex((product) => product.id === id);
+		// 	if (productIndex === -1) {
+		// 		// console.log('update fails: ', productIndex);
+		// 		return new Error();
+		// 	}
+
+		// 	//update data in cart.json
+		// 	if(data[productIndex].price !== updateProduct.price){
+		// 		Cart.updateTotalPrice(productIndex,data[productIndex].price, updateProduct.price);
+		// 	}
+
+		// 	//update lai data
+		// 	data[productIndex] = updateProduct;
+		// 	const newData = [...data];
+
+		// 	// write lai vao file product.json
+		// 	fs.writeFile(p, JSON.stringify(newData), (err) => {
+		// 		console.log(err);
+		// 	});
+		// });
+	}
+
+	static deleteProduct = (id, cb) => {
+		// get product
+		const db = getDb();
+		db.collection("products")
+			.deleteOne({ _id: ObjectId(id)  })
+			.then(() => {
+				console.log('Deleted successfully');
+				cb();
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+
+		// getProductFromFile((data) =>{
+		// 	const newData = [...data];
+		// 	const deleteProduct = newData.find( p => p.id === id);
+		// 	const newProducts = newData.filter(product => product.id !== id);
+		// 	fs.writeFile(p, JSON.stringify(newProducts), (err) => {
+		// 		if(!err) {
+		// 			Cart.deleteProduct(id,+deleteProduct.price);
+		// 		}else{
+		// 			console.log(err);
+		// 		}
+		// 	})
+		// })
 
 		// write back to json file
-	}
+	};
 }
 
 export default Product;
